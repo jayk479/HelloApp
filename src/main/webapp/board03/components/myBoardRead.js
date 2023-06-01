2aff1692fd68b083b203f9b52dcde2029a1550ea
user <user@DESKTOP-ESV96G5>
2023-06-01 17:57:56 +0900

    230601

--- src/main/webapp/board03/components/myBoardRead.js
+++ src/main/webapp/board03/components/myBoardRead.js
@@ -4,14 +4,29 @@ export default {
                   <table id="list">
                     <tr>
                         <td style="width:50px;">글제목</td>
-                        <td>{{ item.title }}</td>
+                        <td>{{ board.title }}</td>
                     </tr>
                     <tr style="height:300px">
-                      <td colspan="2">{{ item.content }}</td>
+                      <td colspan="2">{{ board.content }}</td>
                     </tr>
                   </table>
                   <!--<button style="float:right;" v-on:click="boardList">목록</button>-->
-                  <router-link tag="button" style="float:right" to="/boardList">목록</router-link>
+                  <router-link tag="button" style="float:right" v-bind:to="{name : 'boardList'}">목록</router-link>
               </div>`,
-  props: ['item']
+  data : function(){
+    return{
+      board : {}
+    }
+  },
+  props: ['item'],
+
+  // 단건조회정답
+  created : function(){
+    fetch('http://192.168.0.51:8081/myserver/boardInfo?no=' + this.item.no)
+    .then(response => response.json())
+    .then(data => {
+      this.board = data;
+    })
+    .catch(err => console.log(err));
+  }
 };
